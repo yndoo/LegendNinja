0e99819b2c71036a170d5747a18ba6f822048ed0
hyojeong0509 <41557022+hyojeong0509@users.noreply.github.com>
2025-02-24 10:53:06 +0900

    [ chore ] Player 클래스 생성
    + Player Class 생성

---
+++ Assets/Scripts/Entity/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    public void Move()
+    {
+
+    }
+
+    public override void Attack()
+    {
+        
+        // base.Attack();
+    }
+
+    public void UseSkill()
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
