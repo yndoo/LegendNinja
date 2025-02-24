be8905a03eb67459612e576eda9b14116fc4e358
hyojeong0509 <41557022+hyojeong0509@users.noreply.github.com>
2025-02-24 17:20:05 +0900

    [ Chore ] 태겸님 작업 파일 추가

--- Assets/Scripts/Skill/Weapon/Weapon.cs
+++ Assets/Scripts/Skill/Weapon/Weapon.cs
@@ -1,12 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    public virtual void Attak()
+    // Start is called before the first frame update
+    void Start()
     {
-        Console.WriteLine("기본 무기 데미지");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
