f96d1443d6cc0c984bb6d7dc893c49134829b25f
hyojeong0509 <41557022+hyojeong0509@users.noreply.github.com>
2025-02-27 19:33:49 +0900

    [ Feat ] 플레이어 Canvas Layer 수치조절, 시작화면 UI 중간 저장

---
+++ Assets/Scripts/Manager/SceneManager_.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager_ : MonoBehaviour
+{
+    public void StartScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitScene()
+    {
+        // 게임 종료
+#if UNITY_EDITOR
+        // 에디터에서 실행 중일 경우
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // 빌드에서 실행 중일 경우
+        Application.Quit();
+#endif
+    }
+}
